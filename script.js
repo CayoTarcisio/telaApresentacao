fccdfd481646e807443c78d6efbedcb9936b8f0c
Cayo Tarcisio <cayotarcisio51@gmail.com>
2023-12-21 03:34:42 -0300

    final version

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
+  html.classList.toggle('light')
 
-  html.classList.toggle("light")
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+ 
+  // substituir a imagem 
+  if(html.classList.contains('light')) {
+  // se tiver light mode, adicionar imagem light
+  img.setAttribute('src', './assets/cayoTarcisio.gif')
+  } else {
+  // se tiver sem ligh mode, manter imagem normal
+  img.setAttribute('src', './assets/cayoBraga.gif')
+  }
 }
